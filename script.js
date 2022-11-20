7da81cdb1f505a7c5860485e69f9a53165677063
Junayet Khan <junayetkhanjon@gmail.com>
2022-11-20 09:55:06 -0800

    js added

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ document.getElementById('Generate-Pin').addEventListener("click",function(){
     const GenereteField = document.getElementById('generete-input');
     GenereteField.value = GetPin;
 })
+document.getElementById('calculator-body').addEventListener("click",function(event){
+    const number = event.target.innerText;
+    const getInputField = document.getElementById('calculator-input');
+    const currentFieldNumber = getInputField.value;
+    if (isNaN(number)){
+       if(number === 'C'){
+        getInputField.value = '';
+       } else if(number === '<') {
+        const getNumbers = currentFieldNumber.split('');
+        getNumbers.pop();
+        const newDigit = getNumbers.join('');
+        getInputField.value = newDigit;
+       }
+    } 
+    else{
+        
+        
+        const newFieldnumber = currentFieldNumber + number;
+        getInputField.value = newFieldnumber;
+        // console.log(newFieldnumber);
+    }
+})
